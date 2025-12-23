364e756a6dfe3b77d1afbacada06e9f4acd4c544
artem-totality <artem.totality@gmail.com>
2025-12-23 21:23:52 +0000

    Add DiceSimilarity class

---
+++ ie/atu/sw/DiceSimilarity.java
@@ -0,0 +1,54 @@
+package ie.atu.sw;
+
+import java.util.Set;
+
+public class DiceSimilarity {
+    /**
+     * Calculates Sørensen–Dice similarity
+     * 
+     * @param tokensA - first tokens set
+     * @param tokensB - second tokens set
+     * @return similarity coefficient
+     */
+    public double calculate(
+            Set<String> tokensA,
+            Set<String> tokensB) {
+        return 2 * intersectionSize(tokensA, tokensB) / (tokensA.size() + tokensB.size());
+    }
+
+    /**
+     * Calculates the size of the intersection of two sets
+     * 
+     * @param a - first tokens set
+     * @param b - second tokens set
+     * @return number of common elements
+     */
+    private int intersectionSize(
+            Set<String> a,
+            Set<String> b) {
+
+        var intersection = 0;
+
+        if (a.size() < b.size()) {
+            var iter = a.iterator();
+
+            while (iter.hasNext()) {
+                var token = iter.next();
+
+                if (b.contains(token))
+                    intersection += 1;
+            }
+        } else {
+            var iter = b.iterator();
+
+            while (iter.hasNext()) {
+                var token = iter.next();
+
+                if (a.contains(token))
+                    intersection += 1;
+            }
+        }
+
+        return intersection;
+    }
+}
