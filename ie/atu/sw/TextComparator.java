54517748c7c5de1d7f1dbf31bfe848aed73880b6
artem-totality <artem.totality@gmail.com>
2025-12-23 10:57:47 +0000

    Add TextComparator class

---
+++ ie/atu/sw/TextComparator.java
@@ -0,0 +1,35 @@
+package ie.atu.sw;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TextComparator {
+    Path textFileA = null;
+    Path textFileB = null;
+    Path stopWordsFile = null;
+
+    public void setTextFileA(String textFileAPath) {
+        this.textFileA = Paths.get(textFileAPath);
+    }
+
+    public void setTextFileB(String textFileBPath) {
+        this.textFileB = Paths.get(textFileBPath);
+    }
+
+    public void setStopWordsFile(String stopWordsFilePath) {
+        this.stopWordsFile = Paths.get(stopWordsFilePath);
+    }
+
+    public Path getTextFileA() {
+        return textFileA;
+    }
+
+    public Path getTextFileB() {
+        return textFileB;
+    }
+
+    public Path getStopWordsFile() {
+        return stopWordsFile;
+    }
+
+}
